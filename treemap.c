f921e229f9c2aefe37164f73150d4ec55692caab
Your Name <you@example.com>
2024-05-11 01:08:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -37,19 +37,73 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
+    TreeMap* new = (TreeMap *)malloc(sizeof(TreeMap));
+    new->lower_than = lower_than;
 
-    //new->lower_than = lower_than;
-    return NULL;
+    if (new->lower_than == NULL) return NULL;
+
+    new->root = NULL;
+    return new;
 }
 
+void insertTreeMap(TreeMap * tree, void* key, void * value) 
+{
+    if (tree == NULL) return;
 
-void insertTreeMap(TreeMap * tree, void* key, void * value) {
+    if (tree->root == NULL)
+    {
+        tree->root = createTreeNode(key, value);
+        return;
+    }
 
+    TreeNode* tmp = tree->root;
+
+    while (tmp != NULL)
+        {
+            if (is_equal(tree, key, tmp -> pair -> key))return;
+
+            if (tree -> lower_than(key, tmp -> pair -> key))
+            {
+                TreeNode* aux = createTreeNode(key, value);
+                aux -> parent = tmp;
+                tmp -> left = aux;
+                tree -> current = aux;
+                return;
+            }
+            
+            if (tmp -> left != NULL)
+            {
+                tmp = tmp-> left;
+            }
+        }
+    
+    if (tree -> lower_than(key, tmp -> pair -> key))
+    {
+        if (tmp -> right == NULL)
+        {
+            TreeNode* aux = createTreeNode(key, value);
+            aux -> parent = tmp;
+            tmp -> left = aux;
+            tree -> current = aux;
+            return;
+        }
+        
+        if (tmp -> right != NULL)
+        {
+            tmp = tmp -> right;
+        }
+    }
 }
 
-TreeNode * minimum(TreeNode * x){
+TreeNode * minimum(TreeNode * x)
+{
+    while (x -> left != NULL)
+        {
+            if(x -> left == NULL) break;
 
-    return NULL;
+            x = x -> left;
+        }
+    return x;
 }
 
 
